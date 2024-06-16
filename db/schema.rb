b7ef56981c86ecb2c9ceed0c05ec0e16b2fe19ad
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2024-06-16 19:45:35 -0300

    Add genre and publisher models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_16_224139) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_16_224414) do
   create_table "authors", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -29,6 +29,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_16_224139) do
     t.index ["user_id"], name: "index_genres_on_user_id"
   end
 
+  create_table "publishers", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name", "user_id"], name: "index_publishers_on_name_and_user_id", unique: true
+    t.index ["user_id"], name: "index_publishers_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email"
     t.datetime "created_at", null: false
@@ -37,4 +46,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_16_224139) do
   end
 
   add_foreign_key "genres", "users"
+  add_foreign_key "publishers", "users"
 end
---
+++ spec/factories/publishers.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :publisher do
+    name { "MyString" }
+    user { nil }
+  end
+end
