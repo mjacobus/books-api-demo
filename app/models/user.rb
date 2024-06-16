7ae8c780c97c3dad81fea08b05eda81127b68dfb
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2024-06-16 20:24:22 -0300

    Seed books

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :books
+  has_many :genres
+  has_many :authors
+  has_many :publishers
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,70 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+class UserSeeder
+  attr_reader :user
+
+  def initialize(email:, access_token:)
+    @user = User.find_or_initialize_by(email:) do |user|
+      user.access_token = access_token
+    end
+    @user.save!
+
+    Book.destroy_all
+    Genre.destroy_all
+    Publisher.destroy_all
+    Author.destroy_all
+  end
+
+  def seed
+    seed_genres
+    seed_publishers
+    seed_authors
+    seed_books
+  end
+
+  def seed_authors
+    5.times do
+      user.authors.find_or_create_by(name: Faker::Book.author)
+    end
+
+    puts "Seeded #{Author.count} authors for #{user.email}"
+  end
+
+  def seed_publishers
+    5.times do
+      user.publishers.find_or_create_by(name: Faker::Book.publisher)
+    end
+
+    puts "Seeded #{Publisher.count} publishers for #{user.email}"
+  end
+
+  def seed_genres
+    5.times do
+      user.genres.find_or_create_by(name: Faker::Book.genre)
+    end
+
+    puts "Seeded #{Genre.count} genres for #{user.email}"
+  end
+
+  def author
+    user.authors.sample
+  end
+
+  def publisher
+    user.publishers.sample
+  end
+
+  def genre
+    user.genres.sample
+  end
+
+  def seed_books
+    1.upto(10).each do
+      FactoryBot.create(:book, author:, publisher:, genre:, user:, title: Faker::Book.title)
+    end
+  end
+end
+
+seeder = UserSeeder.new(email: 'john@doe.com', access_token: 'token_one')
+seeder.seed
+
+seeder = UserSeeder.new(email: 'jane@doe.com', access_token: 'token_two')
+seeder.seed
