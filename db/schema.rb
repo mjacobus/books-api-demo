1f37f7f9371b1f63d7209fbeb241608693d2978b
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2024-06-16 19:42:32 -0300

    Add genre model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_16_223041) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_16_224139) do
   create_table "authors", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -20,6 +20,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_16_223041) do
     t.index ["user_id"], name: "index_authors_on_user_id"
   end
 
+  create_table "genres", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name", "user_id"], name: "index_genres_on_name_and_user_id", unique: true
+    t.index ["user_id"], name: "index_genres_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email"
     t.datetime "created_at", null: false
@@ -27,4 +36,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_16_223041) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "genres", "users"
 end
