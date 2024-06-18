3567ee7cf4b250058e764e95a06f8240d93b0bf4
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2024-06-18 18:04:20 -0300

    Rescue argument error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::API
+  rescue_from ArgumentError do |error|
+    argument_error(error)
+  end
+
   private
 
   def current_user
@@ -14,5 +18,22 @@ class ApplicationController < ActionController::API
     handler = klass.new(current_user:)
     response = handler.handle(request)
     render json: response.data, status: response.status, headers: response.headers
+  end
+
+  def argument_error(error)
+    render json: { error: 'Invalid request', backtrace: backtrace(error) }.compact, status: :bad_request
+  end
+
+  def backtrace(error)
+    return unless Rails.env.development?
+
+    error.backtrace.map do |line|
+      line = line.gsub("#{Rails.root}/", '')
+      # replace ommit gem path
+      line = line.gsub(%r{.*/gems/[0-9.]+/}, '{GEM_PATH}/') if line.include?('/gems/')
+
+      line if line.start_with?('app/')
+      # line
+    end.compact
   end
 end
